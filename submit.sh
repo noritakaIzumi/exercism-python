794394c637ce45bf7bd85a0ba6fede52096bc077
Noritaka IZUMI <noritaka.izumi@gmail.com>
2022-01-30 10:00:45 +0900

    fix: submit script

--- submit.sh
+++ submit.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 DIR=$(dirname $(which $0))
-
 NAME=$(echo ${1} | cut -d'/' -f1)
 
-echo ${NAME}
-find ${DIR}/${NAME} | grep -E '[^(_test)]\.py$' | xargs echo exercism submit
+find ${DIR}/${NAME} | grep -E '\.py$' | grep -vE '_test\.py$' | xargs exercism submit
